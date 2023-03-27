648365080db9657daff0f313a28a371120f93e7f
Soduckhee <113624521+soducky@users.noreply.github.com>
2023-03-28 02:44:46 +0900

    03.28

--- CMS_Kiosk/Assets/NetWorkScript/AduinoOFF.cs
+++ CMS_Kiosk/Assets/NetWorkScript/AduinoOFF.cs
@@ -7,19 +7,32 @@ using Unity.VisualScripting;
 
 public class AduinoOFF : MonoBehaviour
 {
-    SerialPort serialPort = new SerialPort("COM"+DataManager.Instance.data.Devel_COM, 9600, Parity.None, 8, StopBits.One);
+    //SerialPort serialPort = new SerialPort("COM"+DataManager.Instance.data.Devel_COM, 9600, Parity.None, 8, StopBits.One);
+    private SerialPort serialPort;
 
-    void Start()
+    private void Start()
     {
-        serialPort.Open();
-        serialPort.ReadTimeout = 1;
+        try
+        {
+            serialPort = new SerialPort("COM" + DataManager.Instance.data.Devel_COM, 9600, Parity.None, 8, StopBits.One);
+            serialPort.Handshake = Handshake.None;
+            serialPort.DtrEnable = false; // DtrEnable을 false로 설정
+            serialPort.ReadTimeout = 1;
+            serialPort.Open();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Failed to open serial port: " + e.Message);
+        }
     }
 
     private void Update()
     {
-        if(serialPort.IsOpen == true)
+        if (serialPort != null && serialPort.IsOpen && serialPort.BytesToRead > 0)
         {
-            if(serialPort.ReadLine() == "1")
+            string line = serialPort.ReadLine();
+
+            if (line == "1")
             {
                 this.gameObject.GetComponent<Server>().AllOff();
             }
@@ -28,13 +41,25 @@ public class AduinoOFF : MonoBehaviour
 
     public void ArduinoOffCommand()
     {
-        serialPort.WriteLine("c");
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            serialPort.WriteLine("c");
+        }
     }
 
     public void ArduinoOnCommand()
     {
-        serialPort.WriteLine("s");
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            serialPort.WriteLine("s");
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            serialPort.Close();
+        }
     }
 }
-
-
